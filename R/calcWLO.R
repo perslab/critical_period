f8ed87e140fa5f930fdbe09c332c8a3c223dd05f
dylanmr <dylanrausch13@gmail.com>
2021-03-03 16:32:44 +0100

    wlo function now takes groups hotspot detection built for continuous variables and can remove edge weights at cutoffs

--- R/calcWLO.R
+++ R/calcWLO.R
@@ -18,7 +18,7 @@
 
 require(tidylo)
 
-.countsperclus <- function(seur_obj, group=NULL) {
+.countsperclus <- function(seur_obj, group=NULL, min.cell=100) {
   if(is.null(group)) {
     vec <- factor(as.character(Idents(seur_obj)))
   } else {
@@ -27,6 +27,7 @@ require(tidylo)
   mat.sparse <- seur_obj@assays$SCT@counts
   mm <- model.matrix(~ 0 + vec)
   colnames(mm) <- paste0("clus_", levels(vec))
+  mm <- mm[,colSums(mm)>min.cell]
   mat.sum <- mat.sparse %*% mm
   keep <- rowSums(mat.sum > 0) >= ncol(mat.sum)/3
   mat.sum <- mat.sum[keep, ]
@@ -34,19 +35,43 @@ require(tidylo)
 }
 
 
-calcWLO <- function(seur_obj, prior, ...) {
+calcWLO <- function(seur_obj, prior, group, id1 = NULL, id2 = NULL, ...) {
 
-  cpg <- .countsperclus(seur_obj, ...)
+  cpg <- .countsperclus(seur_obj, group = NULL, ...)
   
   if(grepl("un", prior)) {
-    cpg %>% 
+    cpg <-
+      cpg %>% 
       as.data.frame() %>%
       rownames_to_column("gene") %>%
       pivot_longer(-gene) %>%
       dplyr::rename(group = name) %>%
-      mutate(group = as.factor(group)) %>%
-      bind_log_odds(set = group, feature = gene, n = value, 
-                    uninformative = TRUE, unweighted = TRUE) -> dat
+      mutate(group = as.factor(group))
+    
+    if(is.null(id1)) {
+      cpg %>% 
+        bind_log_odds(set = group, feature = gene, n = value, 
+                      uninformative = TRUE, unweighted = TRUE) -> dat
+    } else if(!is.null(id1)) {
+      if(is.null(id2)) {
+        cpg %>% 
+          mutate(group = if_else(group == id1, "group_1", "group_2")) %>% 
+          group_by(gene, group) %>% 
+          summarise(value = sum(value)) %>% 
+          bind_log_odds(set = group, feature = gene, n = value, 
+                        uninformative = TRUE, unweighted = TRUE) -> dat
+      } else {
+        cpg %>% 
+          mutate(group = case_when(group %in% id1 ~ "group_1",
+                                   group %in% id2 ~ "group_2",
+                                   T ~ "remove")) %>%
+          group_by(gene, group) %>% 
+          summarise(value = sum(value)) %>% 
+          filter(group!="remove") %>% 
+          bind_log_odds(set = group, feature = gene, n = value, 
+                        uninformative = TRUE, unweighted = TRUE) -> dat
+      }
+    }
   } else {
     cpg %>% 
       as.data.frame() %>%
--- R/detectHotSpot.R
+++ R/detectHotSpot.R
@@ -16,7 +16,7 @@
 library(tidygraph)
 library(ranger)
 
-detectHotSpot <- function(seur_obj, dims, k, col, return_extra, nperm, score="rf", w=2) {
+detectHotSpot <- function(seur_obj, dims, k, col, return_extra, nperm, score="rf", w=2, alg = "fg", weight_cut=0.75) {
   
   print("Building KNN-graph")
   g <- .build_graph(seur_obj, dims = dims, k = k, reduction = "pca")
@@ -25,9 +25,16 @@ detectHotSpot <- function(seur_obj, dims, k, col, return_extra, nperm, score="rf
   print("Converting to igraph object")
   ig <- bluster::makeKNNGraph(x = Embeddings(seur_obj, reduction = "pca")[,c(1:dims)], k=k)
   print("Calculating edge weights")
-  E(ig)$weight <- .calc_edgeweight_factor(preds, ig, w=w, score=score)
+  E(ig)$weight <- .calc_edgeweight(preds, ig, w=w, score=score)
+  ig <- delete_edges(ig, E(ig)[which(E(ig)$weight < quantile(E(ig)$weight, weight_cut))])
   print("Finding communities")
-  ig_res <- igraph::cluster_fast_greedy(ig)
+  if(alg == "fg") {
+    ig_res <- igraph::cluster_fast_greedy(ig)
+  } else if(alg == "walk") {
+    ig_res <- igraph::cluster_walktrap(ig)
+  } else if(alg == "eig") {
+    ig_res <- igraph::cluster_leading_eigen(ig)
+  }
   # assign values to nodes for downstream analysis
   V(ig)$group <- ig_res$membership
   V(ig)$rf_score <- preds$rf_score
@@ -35,11 +42,12 @@ detectHotSpot <- function(seur_obj, dims, k, col, return_extra, nperm, score="rf
   # permute edges for null distribution
   print("Permuting edges and calculating significance")
   perm_edges <- .perm_edges(ig, nperm,  w=w, score=score)
-  sig_cluster <- .calc_significance(ig, perm_edges)
+  sig_cluster <- .calc_significance(ig, perm_edges) 
+  sig_cluster$padj <- p.adjust(sig_cluster$pval)
   # arrange output data
   df <- data.frame(rf_score = preds$rf_scores, binom_score = preds$binom_scores, hs_clus = ig_res$membership) %>% 
     left_join(sig_cluster %>% dplyr::select(clus, padj), by = c("hs_clus" = "clus")) %>% 
-    mutate(padj = if_else(padj > 0.05, NA_character_, paste0(hs_clus, "_sig")))
+    mutate(sig_clus = if_else(padj > 0.05, NA_character_, paste0(hs_clus, "_sig")))
   rownames(df) <- colnames(seur_obj)
   seur_obj <- AddMetaData(seur_obj, df) 
   # decide what to return
@@ -60,61 +68,76 @@ detectHotSpot <- function(seur_obj, dims, k, col, return_extra, nperm, score="rf
 }
 
 .enrich_score <- function(seur_obj, knn, col, dims, features = NULL) {
-
-  vec <- factor(seur_obj[[col]][,1])
-  prop <- as.vector(table(vec) / length(vec))[1]
-  group <- levels(factor(vec))[1]
-  g <- knn$index
-  neighb_mat <- matrix(vec[g], ncol=ncol(g))
-  neighb_mat <- neighb_mat == group
-  bin_scores <- unlist(lapply(rowSums(neighb_mat), function(x) qnorm(pbinom(x, ncol(g)+1, prop))))
-  if(sum(is.infinite(bin_scores)) > 0) {
-    print("infinite scores found, z-scores will be replaced with max value")
-  }
-  bin_scores <- ifelse(is.infinite(bin_scores), max(bin_scores[!is.infinite(bin_scores)]), bin_scores)
-  # build a data frame to predict labels
-  if(is.null(features)) {
-    dat <- data.frame(vec = vec, Matrix::t(seur_obj@assays$SCT@scale.data))
-  } else if(features=="pca") {
-    dat <- data.frame(vec = vec, Embeddings(seur_obj, reduction = "pca")[,c(1:dims)])
-  }
-  # predict labels w/random forest and retain probabilities
-  rf_pred <- predictions(ranger::ranger(vec ~ .,data = dat, probability = T))[,1]
-  # to ensure weights are high for both ends of the spectrum
-  rf_pred <- scales::rescale(rf_pred, c(-1,1))
-  return(list(binom_scores = bin_scores, rf_scores = rf_pred))
   
+  vec <- seur_obj[[col]][,1]
+  g <- knn$index
+  # prediction
+  if(is.numeric(vec)) {
+    # only return random forest predictions if the metadata is continuous
+    if(is.null(features)) {
+      dat <- data.frame(vec = vec, Matrix::t(seur_obj@assays$SCT@scale.data))
+    } else if(features=="pca") {
+      dat <- data.frame(vec = vec, Embeddings(seur_obj, reduction = "pca")[,c(1:dims)])
+    }
+    rf_pred <- predictions(ranger::ranger(vec ~ .,data = dat))
+    return(list(binom_scores = rf_pred, rf_scores = rf_pred))
+  } else {
+    # perform both binomial test and RF prediction
+    vec <- factor(vec)
+    prop <- as.vector(table(vec) / length(vec))[1]
+    group <- levels(factor(vec))[1]
+    neighb_mat <- matrix(vec[g], ncol=ncol(g))
+    neighb_mat <- neighb_mat == group
+    bin_scores <- unlist(lapply(rowSums(neighb_mat), function(x) qnorm(pbinom(x, ncol(g)+1, prop))))
+    if(sum(is.infinite(bin_scores)) > 0) {
+      print("infinite scores found, z-scores will be replaced with max value")
+    }
+    bin_scores <- ifelse(is.infinite(bin_scores), max(bin_scores[!is.infinite(bin_scores)]), bin_scores)
+    if(is.null(features)) {
+      dat <- data.frame(vec = vec, Matrix::t(seur_obj@assays$SCT@scale.data))
+    } else if(features=="pca") {
+      dat <- data.frame(vec = vec, Embeddings(seur_obj, reduction = "pca")[,c(1:dims)])
+    }
+    rf_pred <- predictions(ranger::ranger(vec ~ .,data = dat, probability = T))[,1]
+    rf_pred <- scales::rescale(rf_pred, c(-1,1))
+    return(list(binom_scores = bin_scores, rf_scores = rf_pred))
+  }
 }
   
-.calc_edgeweight_factor <- function(preds, g, w, score) {
-  weights <- 
-    tidygraph::as_tbl_graph(g) %>% 
-    activate(edges) %>% 
-    mutate(score_rf = abs(preds$rf_scores[from] + preds$rf_scores[to])^w,
-           score_bin = abs(preds$binom_scores[from] + preds$binom_scores[to])^w) %>% 
-    as_tibble()
-  
-  if(score == "rf"){
+.calc_edgeweight <- function(preds, g, w, score) {
+  # get edge-weights for continuous variables
+  if(sum(preds$binom_scores) == sum(preds$rf_scores)) {
+    weights <- 
+      tidygraph::as_tbl_graph(g) %>% 
+      activate(edges) %>% 
+      mutate(score_rf = abs(preds$rf_scores[from] - preds$rf_scores[to])^w,
+             score_rf = 1/score_rf) %>% 
+      as_tibble()
+    weights$score_rf <- scales::squish(x = weights$score_rf, range = c(0, quantile(weights$score_rf, .9)), only.finite = F)
     return(weights$score_rf)
-  }else{
-    return(weights$score_bin)
+  } else {
+    # get edge-weights for factor variables
+    weights <- 
+      tidygraph::as_tbl_graph(g) %>% 
+      activate(edges) %>% 
+      mutate(score_rf = abs(preds$rf_scores[from] + preds$rf_scores[to])^w,
+             score_bin = abs(preds$binom_scores[from] + preds$binom_scores[to])^w) %>% 
+      as_tibble()
+    
+    if(score == "rf"){
+      return(weights$score_rf)
+    }else{
+      return(weights$score_bin)
+    }
   }
+  
 }  
 
-.calc_edgeweight_cont <- function(preds, g, w, score) {
-  weights <- 
-    tidygraph::as_tbl_graph(g) %>% 
-    activate(edges) %>% 
-    mutate(score_rf = (preds[from] - preds[to]),
-           score_rf = 1/score_rf^w,
-           score_rf = if_else(score_rf < 0, 0, score_rf)) %>% 
-    as_tibble()
-    return(weights$score_rf)
-}  
-  
+
+
 .perm_edges <- function(g, nperm, score, w) {
   resample_rf <- replicate(nperm, data.frame(rf_scores = sample(V(g)$rf_score), binom_scores = sample(V(g)$binom_score)), simplify = F)
-  perm_edges <- lapply(resample_rf, .calc_edgeweight_factor, g=g, score = score, w=w) %>% 
+  perm_edges <- lapply(resample_rf, .calc_edgeweight, g=g, score = score, w=w) %>% 
     bind_cols() %>% 
     mutate(from = tidygraph::as_tbl_graph(g) %>% 
              activate(edges) %>% 
@@ -142,8 +165,7 @@ detectHotSpot <- function(seur_obj, dims, k, col, return_extra, nperm, score="rf
            sumstats = map(data, function(x) data.frame(mean = mean(x$sum, na.rm=T), sd = sd(x$sum, na.rm=T)))) %>% 
     left_join(true_val) %>% 
     mutate(auc = unlist(map2(ecdf, true_mod, ~.x(.y))),
-           pval = unlist(map2(sumstats, true_mod, ~pnorm(.y, .x$mean, .x$sd, lower.tail = F))),
-           padj = p.adjust(pval)) %>% 
+           pval = unlist(map2(sumstats, true_mod, ~pnorm(.y, .x$mean, .x$sd, lower.tail = F)))) %>% 
     dplyr::select(-data)
 }
 
