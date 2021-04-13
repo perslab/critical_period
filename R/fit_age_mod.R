adae8eae1b3e20293c7412b9520bc0c93efdb66e
dylanmr <dylanrausch13@gmail.com>
2021-04-13 13:42:50 +0200

    updated models

--- R/fit_age_mod.R
+++ R/fit_age_mod.R
@@ -19,69 +19,57 @@ library(Matrix)
 library(progressr)
 handlers(global = TRUE)
 
-# think about markov random fields
-# build a model to get slope of each gene within each cluster
-# by age. Use mgcv to speed up calculations in large data-sets
-# include a random intercept for cluster (we dont care that expression 
-# may start at different levels across groups). Include a random intercept 
-# for hash (individual mouse), litter, and hash pool. Will have to add 
-# a random term for sequencing pool. fixed effect to control for the effect 
-# of sex
+.fit_age <- function(data, family, byclus) {
+  if (byclus == T) {
 
-.fit_cluster_slope_re <- function(data) {
-  bam(value ~ 0 + age:clus + 
-    s(clus, sex, bs="re") +  
-    s(clus, bs="re") +
-    s(hash, bs = "re") +
-    s(litter, bs="re") +
-    s(hpool, bs="re"),
-  data = data, method = "REML", family = nb)
+    # think about markov random fields
+    # build a model to get slope of each gene within each cluster
+    # by age. Use mgcv to speed up calculations in large data-sets
+    # include a random intercept for cluster (we dont care that expression
+    # may start at different levels across groups). Include a random intercept
+    # for hash (individual mouse), litter, and hash pool. Will have to add
+    # a random term for sequencing pool. fixed effect to control for the effect
+    # of sex
+
+    bam(value ~ 0 + age:clus +
+      s(clus, sex, bs = "re") +
+      s(clus, bs = "re") +
+      s(hash, bs = "re") +
+      s(litter, bs = "re") +
+      s(hpool, bs = "re"),
+    data = data, method = "fREML", family = family, discrete = T
+    )
+  } else {
+
+    # build a model to get slope of each gene across all populations by
+    # by age. Use mgcv to speed up calculations in large data-sets
+    # include a random intercept for cluster and a random slope that is not correlated
+    # to the level of the intercept. Include a random intercept
+    # for hash (individual mouse), litter, and hash pool. Will have to add
+    # a random term for sequencing pool. fixed effect to control for the effect
+    # of sex
+
+    bam(value ~ age +
+      s(clus, bs = "re") +
+      s(age, clus, bs = "re") +
+      s(age, sex, bs = "re") +
+      s(hash, bs = "re") +
+      s(litter, bs = "re") +
+      s(hpool, bs = "re"),
+    data = data, method = "fREML", family = family, discrete = T
+    )
+  }
 }
 
-# build a model to get slope of each gene across all populations by
-# by age. Use mgcv to speed up calculations in large data-sets
-# include a random intercept for cluster and a random slope that is not correlated 
-# to the level of the intercept. Include a random intercept 
-# for hash (individual mouse), litter, and hash pool. Will have to add 
-# a random term for sequencing pool. fixed effect to control for the effect 
-# of sex
+# catch errors
+.safe_model <- safely(.fit_age)
 
-.fit_age_re <- function(data) {
-  bam(value ~ age + 
-    s(clus, bs = "re") +
-    s(age, clus, bs = "re") +
-    s(age, sex, bs = "re") +
-    s(hash, bs = "re") +
-    s(litter, bs="re") +
-    s(hpool, bs="re"),
-  data = data, method = "REML", family = nb)
-}
+# format data for modeling
+.get_long_data <- function(seur_obj, nfeat) {
 
-# generate model outputs
-.get_model_summs <- function(x) {
-    age_shared <- .fit_age_re(x)
-    age_spec <- .fit_cluster_slope_re(x)
-    summ_shared <- summary(age_shared)
-    summ_spec <- summary(age_spec)
-    return(tibble(summ_shared = list(summ_shared), summ_spec = list(summ_spec)))
-  }
-
-fit_maturation_model <- function(seur_obj, group = "wt", workers=40, min.pct = .01) {
-  
-  if (group == "chow") {
-    dat <- subset(seur_obj, subset = diet == "CHOW" & geno == "wt")
-  } else if (group == "mut") {
-    dat <- subset(seur_obj, subset = geno == "mut")
-  } else if (group == "hfd") {
-    dat <- subset(seur_obj, subset = diet == "mHFD")
-  } else if (group == "wt") {
-    dat <- subset(seur_obj, subset = geno == "wt") 
-  }
-  
   # filter genes to those which are detected in at least 1% of cells more than once
-  min_cells <- round(min.pct*ncol(dat@assays$SCT@counts))
-  keep_genes <- Matrix::rowSums(dat@assays$SCT@counts > 1) > min_cells
-  
+  var_feats <- .get_var_genes(seur_obj, nfeat)
+
   # only model on clusters with > 100 cells in the arcuate
   not_arc <- paste(paste0("n", c("01", "06", 16:18, 29:31, 33:34)), collapse = "|")
   keep_clus <-
@@ -90,55 +78,92 @@ fit_maturation_model <- function(seur_obj, group = "wt", workers=40, min.pct = .
     filter(n > 100) %>%
     pull(predicted.id)
   keep_clus <- keep_clus[!grepl(not_arc, keep_clus)]
-  
+
+  # filter genes and add metadata to gene expression matrix
+  # get corrected counts from SCT counts matrix to use for modelling
+
   # filter genes and add metadata to gene expression matrix
   data <-
-    dat@assays$SCT@counts[keep_genes,] %>%
+    dat@assays$SCT@counts[keep_genes, ] %>%
     Matrix::t() %>%
-    cbind(dat[[]] %>% 
-            dplyr::select(
-              age, predicted.id, hash_id, sex_call, hash_pool
-              ) %>% 
-            mutate(
-              age = as.numeric(age), 
-              sex = factor(sex_call)
-              )
-          )
+    cbind(dat[[]] %>%
+      dplyr::select(
+        age, predicted.id, hash_id, sex_call, hash_pool
+      ) %>%
+      mutate(
+        age = as.numeric(age),
+        sex = factor(sex_call)
+      ))
 
-  # convert matrix to a nested data.table, each gene is a row and all 
+  # convert matrix to a nested data.table, each gene is a row and all
   # info is nested in the data column. data.table is significantly faster and
   # we run out of memory using dplyr::nest
-  dt.long <- 
-    data %>% 
+  dt.long <-
+    data %>%
     filter(
       predicted.id %in% keep_clus,
       age < 40,
-      age > 0 
-      ) %>% 
+      age > 0
+    ) %>%
     mutate(
       hash = droplevels(hash_id),
       clus = droplevels(factor(predicted.id)),
       litter = droplevels(factor(str_split(hash, "-", simplify = T)[, 1])),
       hpool = droplevels(factor(hash_pool))
-      ) %>% 
+    ) %>%
     dplyr::select(
-      -predicted.id, -hash_id, -sex_call, -hash_pool 
-    ) %>% 
-    data.table::data.table() %>% 
-    tidyfast::dt_pivot_longer(., c(-age, -clus, -hash, -sex, -hpool, -litter)) %>% 
+      -predicted.id, -hash_id, -sex_call, -hash_pool
+    ) %>%
+    data.table::data.table() %>%
+    tidyfast::dt_pivot_longer(., c(-age, -clus, -hash, -sex, -hpool, -litter)) %>%
     tidyfast::dt_nest(., name)
-  
+}
+
+
+# run models
+.run_model <- function(x, family) {
+  age_shared <- .fit_age(x, family, byclus == F)
+  age_spec <- .fit_age(x, family, byclus == T)
+}
+
+# generate model summaries
+.gen_summary <- function(models) {
+  summ_shared <- summary(models[[1]])
+  summ_spec <- summary(models[[2]])
+  return(tibble(summ_shared = list(summ_shared), summ_spec = list(summ_spec)))
+}
+
+
+fit_maturation_model <- function(seur_obj, group = "wt", workers = 40, nfeat = 3000, family) {
+  if (group == "chow") {
+    dat <- subset(seur_obj, subset = diet == "CHOW" & geno == "wt")
+  } else if (group == "mut") {
+    dat <- subset(seur_obj, subset = geno == "mut")
+  } else if (group == "hfd") {
+    dat <- subset(seur_obj, subset = diet == "mHFD")
+  } else if (group == "wt") {
+    dat <- subset(seur_obj, subset = geno == "wt")
+  }
+
+  dt.long <- .get_long_data(dat, nfeat)
+
   print("requesting cores")
-  
-  if(!is.null(workers)) {
+
+  if (!is.null(workers)) {
     plan("multisession", workers = workers)
   } else {
     plan("sequential")
   }
-  
-  print("got cores")
-  
-  shared_age <- furrr::future_map(dt.long$data, .get_model_summs, .progress = T)
 
+  print("got cores")
+
+  age_models <- furrr::future_map(dt.long$data, ~ .run_model(.x, family), .progress = T)  %>%  
+    setNames(dt.long$name) %>% 
+    keep(~is.null(.x$error)) %>% 
+    map(1)
+  
+  model_summs <- furrr::future_map(age_models, .gen_summary) %>% 
+    bind_rows(.id="gene")
+  
   return(shared_age)
 }
--- R/fit_interaction.R
+++ R/fit_interaction.R
@@ -3,137 +3,180 @@
 ##' .. content for \details{} ..
 ##'
 ##' @title
-##' @param data
+##' @param seur_obj seurat object
+##' @param k number of basis functions to fit with GAM
+##' @param family distribution on which to model data
+##' @param ordered should parametric term in the model be ordered
+##' @param basis type of function to model smooth terms
+##' @param nfeat number of variable features to find
+##' 
 ##' @return
 ##' @author dylanmr
 ##' @export
 
-# model expression of all detectable genes in agrp neurons over age. 
-# Use mgcv to speed up calculations in large data-sets. We are using 
+library(mgcv)
+library(tidyverse)
+library(Seurat)
+library(furrr)
+library(future)
+library(Matrix)
+
+# model expression of all detectable genes in agrp neurons over age.
+# Use mgcv to speed up calculations in large data-sets. We are using
 # an ordered factor to calculate "difference smooths". As seen here:
 # https://fromthebottomoftheheap.net/2017/12/14/difference-splines-ii/
-# include a random intercept for cluster litter, hash, and hashed pool. 
+# include a random intercept for cluster litter, hash, and hashed pool.
 # Sex is included as a by factor - need to check if this is the right way to include this
-# Include a random intercept 
+# Include a random intercept
 # for hash (individual mouse), litter, and hash pool.
 
-.int_model_ordered <- function(data, k, basis) {
-  bam(value ~ oGeno +
-    s(age, bs = basis, k = k) +
-    s(age, by = oGeno, bs = basis, k = k) +
-    s(age, sex, bs = "re") +
-    s(litter, bs = "re") +
-    s(hash, bs = "re") +
-    s(hpool, bs="re"),
-  data = data, method = "REML", family = nb
-  )
-}
-
-.int_model_mean <- function(data, k, basis) {
-  bam(value ~ geno +
-        s(age, m = 2, bs = basis, k=k) +
-        s(age, by = oGeno, m = 1, bs = basis, k=k) +
-        s(age, sex, bs = "re") +
-        s(litter, bs = "re") +
-        s(hash, bs = "re") +
-        s(hpool, bs="re"),
-      data = data, method = "REML", family = nb
-  )
-}
-
-
-.get_long_data <- function(seur_obj, min.pct = 0.01) {
-  
-  # filter genes to those which are detected in at least 1% of cells more than once
-  min_cells <- round(min.pct*ncol(seur_obj@assays$SCT@counts))
-  keep_genes <- Matrix::rowSums(seur_obj@assays$SCT@counts > 1) > min_cells
-  
-  
-  # filter genes and add metadata to gene expression matrix
-  data <-
-    seur_obj@assays$SCT@counts[keep_genes,] %>%
-    Matrix::t() %>%
-    cbind(seur_obj[[]] %>% 
-            dplyr::select(
-              age, predicted.id, hash_id, sex_call, hash_pool, diet, geno
-            ) %>% 
-            mutate(
-              age = as.numeric(age), 
-              sex = factor(sex_call)
-            )
+.interaction_model <- function(data, k, basis, family, ordered) {
+  if (ordered == T) {
+    bam(value ~ oGeno +
+      s(age, bs = basis, k = k, m=2) +
+      s(age, by = oGeno, bs = basis, k = k, m=1) +
+      s(age, sex, bs = "re") +
+      s(litter, bs = "re") +
+      s(hash, bs = "re") +
+      s(hpool, bs = "re"),
+    data = data, family = family, method="fREML", discrete=T
     )
-  
-  # convert matrix to a nested data.table, each gene is a row and all 
+  } else {
+    bam(value ~ geno +
+      s(age, m = 2, bs = basis, k = k) +
+      s(age, by = oGeno, m = 1, bs = basis, k = k) +
+      s(age, sex, bs = "re") +
+      s(litter, bs = "re") +
+      s(hash, bs = "re") +
+      s(hpool, bs = "re"),
+    data = data, method = "fREML", family = family, discrete=T
+    )
+  }
+}
+
+# catch errors
+.safe_model <- safely(.interaction_model)
+
+# get variable genes for subsetted object and
+# remove non-coding genes
+.get_var_genes <- function(seur_obj, nfeat) {
+  keep_genes <- !grepl("Rik", rownames(seur_obj))
+  seur_obj <- subset(seur_obj, features = rownames(seur_obj)[keep_genes])
+  x <- FindVariableFeatures(seur_obj, nfeatures = nfeat)
+  x <- x@assays$SCT@var.features
+  return(x)
+}
+
+# format data for modeling
+.get_long_data <- function(seur_obj, nfeat) {
+
+  # filter genes to those which are detected in at least 1% of cells more than once
+  var_feats <- .get_var_genes(seur_obj, nfeat)
+
+  # filter genes and add metadata to gene expression matrix
+  # get corrected counts from SCT counts matrix to use for modelling
+  data <-
+    seur_obj@assays$SCT@counts[var_feats, ] %>%
+    Matrix::t() %>%
+    cbind(seur_obj[[]] %>%
+      dplyr::select(
+        age, predicted.id, hash_id, sex_call, hash_pool, diet, geno
+      ) %>%
+      mutate(
+        age = as.numeric(age),
+        sex = factor(sex_call)
+      ))
+
+  # convert matrix to a nested data.table, each gene is a row and all
   # info is nested in the data column. data.table is significantly faster and
   # we run out of memory using dplyr::nest
-  dt.long <- 
-    data %>% 
+  dt.long <-
+    data %>%
     filter(
       age < 40,
-      age > 0 
-    ) %>% 
+      age > 0
+    ) %>%
     mutate(
       geno = factor(geno),
-      oGeno = factor(geno, levels = c("wt", "mut"), ordered=T),
+      oGeno = factor(geno, levels = c("wt", "mut"), ordered = T),
       group = factor(case_when(
         diet == "HFD" ~ "mHFD",
         geno == "mut" ~ "ob/ob",
-        T ~ "gWT")),
+        T ~ "gWT"
+      )),
       oGroup = factor(group, ordered = T),
       hash = droplevels(hash_id),
       clus = droplevels(factor(predicted.id)),
       litter = droplevels(factor(str_split(hash, "-", simplify = T)[, 1])),
       hpool = droplevels(factor(hash_pool))
-    ) %>% 
+    ) %>%
     dplyr::select(
-      -predicted.id, -hash_id, -sex_call, -hash_pool, -diet 
-    ) %>% 
-    data.table::data.table() %>% 
-    tidyfast::dt_pivot_longer(., c(-age, -clus, -hash, -sex, 
-                                   -hpool, -litter, -group, -oGroup,
-                                   -geno, -oGeno)) %>% 
+      -predicted.id, -hash_id, -sex_call, -hash_pool, -diet
+    ) %>%
+    data.table::data.table() %>%
+    tidyfast::dt_pivot_longer(., c(
+      -age, -clus, -hash, -sex,
+      -hpool, -litter, -group, -oGroup,
+      -geno, -oGeno
+    )) %>%
     tidyfast::dt_nest(., name)
 }
 
+# generate predictions from GAM models
+
 .gen_preds <- function(data, model) {
   new_data <- tidyr::expand(data, nesting(hash, age, litter, hpool, oGeno, sex))
-  preds <- bind_cols(new_data, as.data.frame(predict(model, newdata = new_data, se.fit = TRUE,
-                                                     type="response",  
-                                                     exclude = c("s(hash)", "s(litter)",
-                                                                 "s(hpool)", "s(age):sexM",
-                                                                 "s(age):sexF"))))
+  preds <- bind_cols(new_data, as.data.frame(predict(model,
+    newdata = new_data, se.fit = TRUE,
+    type = "response",
+    exclude = c(
+      "s(hash)", "s(litter)", "s(hpool)", "s(age,sex)"
+    )
+  )))
   return(preds)
 }
 
-# generate model outputs
-.get_intmodel_summs <- function(x,k,basis) {
-  int_gam <- .int_model_ordered(x, k, basis)
-  int_par <- .int_model_mean(x, k, basis)
-  summ_gam <- summary(int_gam)
-  summ_par <- summary(int_par)
-  preds <- .gen_preds(data = x, model = int_gam)
-  diff_gam <- tidymv::get_smooths_difference(int_gam, age, list(oGeno = c("mut", "wt")), exclude_random = F)
+# run models
+.run_model <- function(x, k, basis, family) {
+  int_gam <- .safe_model(x, k, basis, family, ordered = T) 
+  int_par <- .safe_model(x, k, basis, family, ordered = F) 
+  return(list(int_gam, int_par))
+}
+
+# generate model summaries
+.gen_summary <- function(int_ord, int_mean, metadata) {
+  summ_gam <- summary(int_ord)
+  summ_par <- summary(int_mean)
+  preds <- .gen_preds(data = metadata, model = models[[1]])
+  diff_gam <- tidymv::get_smooths_difference(models[[1]], age, list(oGeno = c("mut", "wt")), exclude_random = F)
   return(tibble(summ_gam = list(summ_gam), summ_par = list(summ_par), diff_gam = list(diff_gam), preds = preds))
 }
 
 
-fit_interaction <- function(dat, workers, k, basis) {
-  
+fit_interaction <- function(seur_obj, workers, k, basis, family, nfeat = 3000) {
+
   # filter genes to those which are detected in at least 1% of cells more than once
   # filter genes and add metadata to gene expression matrix
-  dt.long <- .get_long_data(dat, min.pct = 0.01)
-  
+  dt.long <- .get_long_data(seur_obj, nfeat)
+
   print("requesting cores")
-  
-  if(!is.null(workers)) {
+
+  if (!is.null(workers)) {
     plan("multisession", workers = workers)
   } else {
     plan("sequential")
   }
-  
-  print("got cores")
-  
-  int_age <- furrr::future_map(dt.long$data, ~.get_intmodel_summs(.x, k, basis), .progress = T)
 
-  return(int_age)
+  print("got cores")
+
+  interaction_ordered <- furrr::future_map(dt.long$data, ~ .safe_model(.x, k, basis, family, ordered = T), .progress = T) %>% 
+    setNames(dt.long$name) %>% 
+    keep(~is.null(.x$error)) %>% 
+    map(1)
+  interaction_mean <- furrr::future_map(dt.long$data, ~ .safe_model(.x, k, basis, family, ordered = F), .progress = T) %>% 
+    setNames(dt.long$name) %>% 
+    keep(~is.null(.x$error)) %>% 
+    map(1)
+  
+  return(interaction_models)
 }
