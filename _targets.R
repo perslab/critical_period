fcf8ade2ee234a27ff638f2e4cbd305171e37fd6
dylanmr <dylanrausch13@gmail.com>
2022-03-03 14:50:39 +0100

    pipeline split up into sections to allow for parallelization of early steps

--- _targets.R
+++ _targets.R
@@ -1,90 +1,208 @@
 library(targets)
 library(tarchetypes)
-library(tidyverse)
+library(future)
+library(future.callr)
 
 options(tidyverse.quiet = TRUE)
-future::plan(future::multisession)
+plan(callr)
 
 tar_option_set(packages = c(
   "mclust", "Seurat", "tidyverse", "tidymodels", "yardstick", "R2HTML",
-  "DropletUtils", "Matrix", "rjson", "googlesheets4", "stringr", "future",
+  "DropletUtils", "Matrix", "rjson", "stringr", "future", "doParallel",
   "org.Mm.eg.db", "scDblFinder", "AnnotationDbi", "SingleCellExperiment",
-  "scran", "scater", "glmGamPoi", "miloR", "BiocNeighbors", "data.table")
+  "scran", "scater", "glmGamPoi", "miloR", "BiocNeighbors", "data.table",
+  "magrittr")
 )
 
-# define parameters for static branching for clustering
+dev <- function(obj, ageset) {
+  CreateSeuratObject(counts = counts(obj), meta.data = colData(obj) %>% as.data.frame) %>% 
+    subset(., subset = age < ageset) %>% 
+    sctClust(dims=40) %>% 
+    mapCamp() %>% 
+    classifySex()
+}
 
-clustering_params <- tidyr::expand_grid(
-  dims = seq(20, 50, by = 10),
-  k = seq(5, 30, by = 5),
-  weight = c("jaccard", "rank"),
-  clus_method = c("louvain", "walktrap")
-) %>%
-  unite("name", clus_method, weight, dims, k, remove = F)
+lep <- function(obj, ageset) {
+  CreateSeuratObject(counts = counts(obj), meta.data = colData(obj) %>% as.data.frame) %>% 
+    subset(., subset = age > ageset & treatment != "NA") %>% 
+    sctClust(dims=40) %>% 
+    mapCamp() %>% 
+    classifySex()
+}
+
+values <- tibble::tibble(
+  method_function = rlang::syms(c("dev", "lep"))
+  )
 
 ## Load your R files
 lapply(list.files("./R", full.names = TRUE), source)
 
-# ensure metadata is up to date
-tar_pipeline(
-  tar_target(exp_paths, path_to_data(), iteration = "list"),
-  tar_target(exp_files, exp_paths, format = "file", pattern = map(exp_paths)),
-  tar_target(hto_paths, path_to_data(type = "hto"), iteration = "list"),
-  tar_target(hto_files, hto_paths, format = "file", pattern = map(hto_paths)),
-  tar_target(web_summaries, gen_summary(files = exp_files), format = "file", pattern = map(exp_files)),
-  tar_target(sce_objects, build_sce_w_hto(exp_files, hto_files), pattern = map(exp_files, hto_files), iteration = "list"),
-  tar_render_rep(
-    report,
-    "qc_report.Rmd",
-    params = tibble(
-      par = c(1:29),
-      output_file = paste0("sc_qc_", 1:29, ".html"))
-  ),
-  tar_target(
-    sc_comb,
-    comb_and_filter(sce_objects) %>% 
-      sctClust(dims=40) %>% 
-      mapCamp() %>% 
-      classifySex()
-  ),
-  tar_target(
-    split_seur,
-    list(subset(sc_comb, subset=predicted.id=="neuron") %>% 
-           sctClust(dims=40) %>% 
-           mapCamp(class="neur"), 
-         subset(sc_comb, subset=predicted.id!="neuron") %>% 
-           sctClust(dims=40) %>% 
-           mapCamp())
-  ),
-  tar_target(
-    velo_obj,
-    comb_and_filter(sce_objects, keep_alt=T)
-  ),
+preprocessed <- 
+  list(
+    tarchetypes::tar_files(
+      hash,
+      path_to_data(proj_num = "SCOP_37", type = "kite")
+    ),
+    tarchetypes::tar_files(
+      exp,
+      path_to_data(proj_num = "SCOP_37", type = "kallisto")
+    ),
+    tar_target(
+      metapath, 
+      path_to_meta(), 
+      format="file"
+    ),
+    tar_target(
+      meta, 
+      readxl::read_excel(metapath) %>% 
+        janitor::clean_names() %>% 
+        mutate(kitv = factor(x10x_version))
+    ),
+    tar_target(
+      web_summaries, 
+      gen_summary(files = exp), 
+      format = "file", 
+      pattern = map(exp)
+    ),
+    tar_target(
+      sce_objects, 
+      build_sce_w_hto(exp, hash), 
+      pattern = map(exp, hash), 
+      iteration = "list"
+    ),
+    tar_target(
+      param_df, 
+      tibble::tibble(par = c(1:44)) %>% 
+        dplyr::mutate(output_file = here::here(paste0("output/qc/hto_demux_qc", 1:n(), ".html")))
+    ),  
+    #tar_render_rep(
+    #  rendered_report,
+    #  "qc_report.Rmd",
+    #  params = param_df
+   # ),
+    tar_target(
+      mergedsce,
+      comb_and_filter(sce_objects, meta, exp)
+    )
+  )
+    
+seuratobjects <- 
+  tar_map(
+    values = values,
+    tar_target(
+      fulldata,
+      method_function(mergedsce, ageset = 35)
+    ),
+    tar_target(
+      neuron, 
+      subset(fulldata, subset = predicted.id == "neuron") %>% 
+        sctClust(dims=40) %>% 
+        mapCamp(class="neuron")
+    ),
+    tar_target(
+      glia, 
+      subset(fulldata, subset = predicted.id != "neuron") %>% 
+        sctClust(dims=40) %>% 
+        mapCamp(class="glia")
+    ),
+    tar_target(
+      milo_neuron,
+      gen_milo(neuron)
+    ),
+    tar_target(
+      milo_glia,
+      gen_milo(glia)
+    )
+  )
+
+substates <- 
+  list(
+    tar_target(
+      milo_agrp,
+      subset(neuron_dev, idents = c(1,6)) %>% 
+        sctClust(dims=20) %>% 
+        gen_milo(., k=30, d=20)
+    ),
+    tar_target(
+      milo_pomc,
+      subset(neuron_dev, idents = c(4)) %>% 
+        sctClust(dims=20) %>% 
+        gen_milo(., k=30, d=20)
+    )
+  )
+
+modeling <-
+  list(
+    tar_target(
+      split_neuron,
+      splitall(neuron_dev, grantdata = grantdata),
+      iteration="list"
+    ),
+    tar_target(
+      paramsformodel,
+      paramsweep(split_neuron),
+      pattern = map(split_neuron),
+      iteration="list"
+    ),
+    tar_target(
+      grantdata,
+      qs::qread("data/grantdata.qs")
+    ),
+    tar_target(
+      neuronpreds,
+      predictage(devdata = split_neuron, lepdata = neuron_lep, grantdata = grantdata, lasso_params = paramsformodel),
+      pattern = map(split_neuron, paramsformodel),
+      iteration="list"
+    )
+  )
+
+
+list(preprocessed, seuratobjects)#, modeling, substates)
+
+  # tar_target(
+  #   seurbycluster,
+  #   splitall(neuron, glia),
+  #   iteration="list"
+  # ),
+  # tar_target(
+  #   lasso_params,
+  #   paramsweep(seurbycluster),
+  #   pattern = map(seurbycluster)
+  # ),
+  #  tar_target(
+  #    predicted_age,
+  #    predage(seur, lasso_params),
+  #    pattern = map(seurbycluster, lasso_params)
+  #  )
+
+
+  # ),
+  # 
   # tar_target(
   #   milo, 
   #   gen_milo(x = split_seur, k=30, d=30),
   #   pattern = map(split_seur), 
   #   iteration="list"
   # ),
-  tar_target(
-    pseudobulk,
-    Seurat::as.SingleCellExperiment(split_seur[[1]], assay="SCT") %>%
-      scater::aggregateAcrossCells(., use.assay.type="counts", id=colData(.)[,c("predicted.id", "hash_id")]),
-    pattern = map(split_seur), 
-    iteration="list"
-  ),
-  tar_target(
-    knn_graph,
-    build_graph(split_seur[[1]]),
-    pattern = map(split_seur), 
-    iteration="list"
-  ),
-  tar_target(
-    resampled_graph,
-    knn_resampling(knn_graph, k = 30, meta = split_seur[[1]][[]]),
-    pattern = map(knn_graph, split_seur),
-    iteration = "list"
-  )
+  # tar_target(
+  #   pseudobulk,
+  #   Seurat::as.SingleCellExperiment(split_seur[[1]], assay="SCT") %>%
+  #     scater::aggregateAcrossCells(., use.assay.type="counts", id=colData(.)[,c("predicted.id", "hash_id")]),
+  #   pattern = map(split_seur), 
+  #   iteration="list"
+  # ),
+  # tar_target(
+  #   knn_graph,
+  #   build_graph(split_seur[[1]]),
+  #   pattern = map(split_seur), 
+  #   iteration="list"
+  # ),
+  # tar_target(
+  #   resampled_graph,
+  #   knn_resampling(knn_graph, k = 30, meta = split_seur[[1]][[]]),
+  #   pattern = map(knn_graph, split_seur),
+  #   iteration = "list"
+  # )
   # tar_target(
   #   integ_with_embr,
   #   build_ds("/data/pub-others/huisman-natcom-2019/GSE126480_Raw_counts.txt.gz") %>% 
@@ -99,4 +217,5 @@ tar_pipeline(
   #     cluster_across_params(sce_final, dims = dims, k = k, weight = weight, clus_method = clus_method)
   #   )
   #)
-)
+
+
